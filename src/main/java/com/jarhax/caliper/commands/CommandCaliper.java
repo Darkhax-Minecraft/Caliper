3d880a961bd61f8f25da92f6a35504d2181bc60b
NotAModder <modderiamnot@gmail.com>
2017-08-10 21:14:15 -0600

    Added a command to find empty/broken recipes.

--- src/main/java/com/jarhax/caliper/commands/CommandCaliper.java
+++ src/main/java/com/jarhax/caliper/commands/CommandCaliper.java
@@ -4,29 +4,30 @@ import net.darkhax.bookshelf.command.CommandTree;
 import net.minecraft.command.ICommandSender;
 
 public class CommandCaliper extends CommandTree {
-
+    
     public CommandCaliper () {
-
+        
         this.addSubcommand(new CommandTeleport());
         this.addSubcommand(new CommandCount());
         this.addSubcommand(new CommandTPS());
+        this.addSubcommand(new CommandEmptyRecipe());
     }
-
+    
     @Override
     public int getRequiredPermissionLevel () {
-
+        
         return 0;
     }
-
+    
     @Override
     public String getName () {
-
+        
         return "caliper";
     }
-
+    
     @Override
     public String getUsage (ICommandSender sender) {
-
+        
         return "commands.caliper.usage";
     }
 }
