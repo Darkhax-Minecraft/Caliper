89e7b1c954aa8d8cbecdb08da633f2e0e4ceb01d
Tyler Hancock <darklime@live.ca>
2017-09-05 16:59:47 -0600

    Added jar signing.

--- src/main/java/com/jarhax/caliper/Caliper.java
+++ src/main/java/com/jarhax/caliper/Caliper.java
@@ -1,26 +1,26 @@
 package com.jarhax.caliper;
 
-import org.apache.logging.log4j.Logger;
-
 import com.jarhax.caliper.commands.CommandCaliper;
 import com.jarhax.caliper.proxy.CommonProxy;
 
+import net.darkhax.bookshelf.lib.LoggingHelper;
 import net.darkhax.bookshelf.registry.RegistryHelper;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = Caliper.MODID, name = Caliper.NAME, version = "@VERSION@", dependencies = "required-after:bookshelf@[2.1.427,)", acceptedMinecraftVersions = "[1.12,1.12.2)", acceptableRemoteVersions = "*")
+@Mod(modid = Caliper.MODID, name = Caliper.NAME, version = "@VERSION@", dependencies = "required-after:bookshelf@[2.1.427,)", acceptedMinecraftVersions = "[1.12,1.12.2)", acceptableRemoteVersions = "*",  certificateFingerprint = "@FINGERPRINT@")
 public class Caliper {
 
     public static final String MODID = "caliper";
     public static final String NAME = "Caliper";
-    public static Logger log;
+    public static final LoggingHelper LOG = new LoggingHelper("Caliper");
     public static RegistryHelper helper;
 
     @SidedProxy(clientSide = "com.jarhax.caliper.proxy.ClientProxy", serverSide = "com.jarhax.caliper.proxy.ServerProxy")
@@ -29,7 +29,6 @@ public class Caliper {
     @Mod.EventHandler
     public void preInit (FMLPreInitializationEvent event) {
 
-        log = event.getModLog();
         helper = new RegistryHelper(MODID).setTab(new CreativeTabCaliper());
         proxy.preInit(event);
         MinecraftForge.EVENT_BUS.register(this);
@@ -51,5 +50,12 @@ public class Caliper {
     public void serverStarting (FMLServerStartingEvent event) {
 
         event.registerServerCommand(new CommandCaliper());
+    }
+    
+    
+    @EventHandler
+    public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
+        
+        LOG.warn("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported by the author!");
     }
 }
