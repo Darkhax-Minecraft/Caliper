42aeccf1b545539c4751201a02e4fc1d173cb5d6
Tyler Hancock <darklime@live.ca>
2017-09-22 16:09:05 -0600

    Added a way to print warnings and added some entity debug utils.

--- src/main/java/com/jarhax/caliper/MiscUtils.java
+++ src/main/java/com/jarhax/caliper/MiscUtils.java
@@ -1,6 +1,16 @@
 package com.jarhax.caliper;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.commons.lang3.text.WordUtils;
+
+import net.minecraft.entity.Entity;
 import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.common.ModContainer;
+import net.minecraftforge.fml.common.registry.EntityEntry;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class MiscUtils {
 
@@ -25,5 +35,53 @@ public class MiscUtils {
     public static int getDimId (WorldServer world) {
 
         return world.provider != null ? world.provider.getDimension() : 0;
+    }
+    
+    public static void bigWarning(boolean trace, List<String> lines) {
+    	
+        Caliper.LOG.error("************************************************************");
+
+        for (String line : lines) {
+        	
+        	for (String subline : wrapString(line, 58, false, new ArrayList<String>())) {
+        		
+        		Caliper.LOG.error("* " + subline);
+        	}
+        }
+        
+    	if (trace) {
+    		
+    		StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+    		
+            for (int i = 2; i < 8 && i < stackTrace.length; i++)
+            {
+                Caliper.LOG.warn("*  at {}{}", stackTrace[i].toString(), i == 7 ? "..." : "");
+            }
+    	}
+    	
+        Caliper.LOG.error("************************************************************");
+    }
+    
+    public static EntityEntry getEntityEntry(Class<? extends Entity> clazz) {
+    	
+    	for (EntityEntry entry : ForgeRegistries.ENTITIES) {
+    		
+    		if (entry.getEntityClass() == clazz) {
+    			
+    			return entry;
+    		}
+    	}
+    	
+    	return null;
+    }
+    
+    public static List<String> wrapString (String string, int lnLength, boolean wrapLongWords, List<String> list) {
+        
+        String lines[] = WordUtils.wrap(string, lnLength, null, wrapLongWords).split(SystemUtils.LINE_SEPARATOR);
+        
+        for (String line : lines)
+            list.add(line);
+            
+        return list;
     }
 }
